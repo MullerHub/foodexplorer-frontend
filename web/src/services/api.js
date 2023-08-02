06c64c33aa419080824222a83f23abac1b66268d
Murilo Da Silva Muller <murilomuller@protonmail.com>
2023-08-01 22:30:41 -0300

    Update api.js

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://food-explorer-back-end-api.onrender.com/'
+  baseURL: 'https://foodexplorerback-end-production.up.railway.app/'
   // baseURL : 'http://localhost:3333/' // para teste
 })
